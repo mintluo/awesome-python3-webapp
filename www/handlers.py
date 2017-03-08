a1bf1cba398721898f882a106215ad594b3847c6
milletluo <lm409@hotmail.com>
2017-03-08 23:57:50 +0800

    完成Day7代码

--- www/handlers.py
+++ www/handlers.py
@@ -3,6 +3,18 @@
 
 __author__ = 'milletluo'
 
-'''
-url handlers.
-'''
+' url handlers '
+
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
